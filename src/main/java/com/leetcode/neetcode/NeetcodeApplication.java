76435d08eef18eea7973ea9e3157eab706f1071f
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-02-24 15:11:03 +0900

    first commit

---
+++ src/main/java/com/leetcode/neetcode/NeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.leetcode.neetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeetcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leetcode/neetcode/NeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.neetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeetcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
